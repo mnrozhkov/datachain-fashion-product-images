46a6355108beb00d9f4ff57835d06438b5aea24c
Mikhail <mnrozhkov@gmail.com>
2024-07-16 16:19:48 +0200

    Initial commit

---
+++ scripts/1-quick-start.py
@@ -0,0 +1,56 @@
+"""
+# Getting Started with DataChain
+
+Before you begin, ensure you have
+- DataChain installed in your environment.
+- Download the Fashion Product Images (Small) dataset (see README.md)
+- Save data in the  `data` directory
+    - `data/images`
+    - `data/styles.csv`
+"""
+
+from datachain import C, DataChain
+
+# Define the paths
+
+DATA_PATH = "gs://datachain-demo/fashion-product-images"
+ANNOTATIONS_PATH = "gs://datachain-demo/fashion-product-images/styles_clean.csv"
+
+# Create a Dataset
+
+print("\n# Create a Dataset")
+ds = DataChain.from_storage(DATA_PATH, type="image").filter(C.name.glob("*.jpg"))
+ds.show(3)
+
+# Create a metadata DataChain
+
+print("\n# Create a metadata DataChain")
+ds_meta = DataChain.from_storage(ANNOTATIONS_PATH).parse_csv().save()
+ds_meta = ds_meta.map(filename=lambda c0: str(c0) + ".jpg", output=str)
+ds_meta.show(3)
+
+# Merge the original image and metadata datachains
+
+print("\n# Merge the original image and metadata datachains")
+ds_annotated = ds.merge(ds_meta, on="name", right_on="filename")
+
+# Save dataset
+
+print("\n# Save dataset")
+ds_annotated.save("fashion-product-images")
+
+
+# Filtering Data
+
+print("\n# Filtering Data")
+ds = (
+    DataChain.from_dataset(name="fashion-product-images")
+    .filter(C.mastercategory == "Apparel")
+    .filter(C.subcategory == "Topwear")
+    .filter(C.season == "Summer")
+)
+print(ds.to_pandas().shape)
+
+
+# NOTE: DataChain requires the  Last line to be an instance of DatasetQuery
+ds.limit(3)
---
+++ scripts/2-embeddings.py
@@ -0,0 +1,41 @@
+import torch
+from torchvision import transforms
+from torchvision.models import resnet50
+
+from datachain import DataChain
+from datachain.lib.image import convert_image
+
+# Model & Transform methods
+
+model = resnet50(pretrained=True).eval()
+transformer = transforms.Compose(
+    [
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
+
+
+# Embeddings processor function
+
+
+def embeddings_processor(file) -> list[float]:
+    img_raw = file.get_value()
+    img = convert_image(img_raw, transform=transformer).unsqueeze(0)
+    with torch.no_grad():
+        emb = model(img)
+
+    return emb[0].tolist()
+
+
+# Compute and Save Embeddings
+
+print("\n# Compute and Save Embeddings:")
+ds_emb = (
+    DataChain(name="fashion-test")
+    .limit(1000)
+    .map(embeddings=embeddings_processor)
+    .save("fashion-embeddings")
+)
+ds_emb.limit(3)
